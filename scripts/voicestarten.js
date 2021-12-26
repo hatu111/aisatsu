5a0f02af87b3a50645ddba182cc855e76f5fc690
hatu111 <93256791+hatu111@users.noreply.github.com>
2021-12-26 12:55:42 +0900

    Add files via upload

---
+++ scripts/voicestarten.js
@@ -0,0 +1,32 @@
+  const startBtn = document.querySelector('#voice-start');
+  const stopBtn = document.querySelector('#voice-stop');
+  const resultDiv = document.querySelector('#voice-here');
+
+  SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+  let recognition = new SpeechRecognition();
+
+  recognition.lang = 'en-US';
+  recognition.interimResults = true;
+  recognition.continuous = true;
+
+  let finalTranscript = ''; 
+
+  recognition.onresult = (event) => {
+    let interimTranscript = ''; 
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+      let transcript = event.results[i][0].transcript;
+      if (event.results[i].isFinal) {
+        finalTranscript += transcript;
+      } else {
+        interimTranscript = transcript;
+      }
+    }
+    resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
+  }
+
+  startBtn.onclick = () => {
+    recognition.start();
+  }
+  stopBtn.onclick = () => {
+    recognition.stop();
+  }
